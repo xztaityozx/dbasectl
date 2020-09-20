db33f2817a335f2ea2d797cd34c320548c782742
xztaityozx <taityonohibi@gmail.com>
2020-09-20 20:41:58 +0900

    HelloWorld

---
+++ cmd/root.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "docbase",
+	Short: "CLI tool for DocBase API",
+	Long:  `CLI tool for DocBase API.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello")
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		logrus.Fatal(err)
+	}
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/xztaityozx/docbase-cli/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
