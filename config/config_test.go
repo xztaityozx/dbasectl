257e791b18914a1a24709d45a7a86ed576d137dd
xztaityozx <taityonohibi@gmail.com>
2020-09-22 11:12:10 +0900

    コンフィグを追加する (#3)
    * configを追加
    
    * go mod更新
    
    * コンフィグ用のオプション追加

---
+++ config/config_test.go
@@ -0,0 +1,94 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/pelletier/go-toml"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v2"
+)
+
+func Test_Load(t *testing.T) {
+	as := assert.New(t)
+	cfg := Config{
+		AccessToken: "Token",
+		TeamName:    "Name",
+	}
+
+	home, _ := homedir.Dir()
+
+	t.Run("load on linux/macOS/windows", func(t *testing.T) {
+		baseDir := filepath.Join(home, ".config", "dbasectl")
+		os.MkdirAll(baseDir, 0755)
+		for _, v := range []string{"json", "yaml", "toml"} {
+			path := filepath.Join(baseDir, fmt.Sprintf("dbasectl.%s", v))
+			var data []byte
+			if v == "json" {
+				data, _ = json.Marshal(cfg)
+			} else if v == "yaml" {
+				data, _ = yaml.Marshal(cfg)
+			} else if v == "toml" {
+				data, _ = toml.Marshal(cfg)
+			}
+
+			as.Nil(ioutil.WriteFile(path, data, 0644))
+
+			{
+				res, err := Load(path)
+				as.Nil(err)
+				as.Equal(cfg, res, "パスを指定してロードできる")
+			}
+
+			{
+				res, err := Load("")
+				as.Nil(err)
+				as.Equal(cfg, res, "パスを指定しなくてもロードできる")
+			}
+		}
+
+		os.RemoveAll(baseDir)
+	})
+
+	t.Run("load on windows", func(t *testing.T) {
+		if runtime.GOOS != "windows" {
+			t.Skip()
+		}
+
+		baseDir := filepath.Join(home, "AppData", "Roaming", "dbasectl")
+		os.MkdirAll(baseDir, 0755)
+		for _, v := range []string{"json", "yaml", "toml"} {
+			path := filepath.Join(baseDir, fmt.Sprintf("dbasectl.%s", v))
+			var data []byte
+			if v == "json" {
+				data, _ = json.Marshal(cfg)
+			} else if v == "yaml" {
+				data, _ = yaml.Marshal(cfg)
+			} else if v == "toml" {
+				data, _ = toml.Marshal(cfg)
+			}
+
+			as.Nil(ioutil.WriteFile(path, data, 0644))
+
+			{
+				res, err := Load(path)
+				as.Nil(err)
+				as.Equal(cfg, res, "パスを指定してロードできる")
+			}
+
+			{
+				res, err := Load("")
+				as.Nil(err)
+				as.Equal(cfg, res, "パスを指定しなくてもロードできる")
+			}
+		}
+
+		os.RemoveAll(baseDir)
+	})
+}
